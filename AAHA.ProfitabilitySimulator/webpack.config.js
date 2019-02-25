025898b22511a3f0c78c6936fd4b70b2039d991e
Gabriel Jimenez <gabe@soonie.net>
2019-02-25 11:51:05 -0700

    Bug fixes/addons per Karie's email sent 02/22/2019

--- AAHA.ProfitabilitySimulator/webpack.config.js
+++ AAHA.ProfitabilitySimulator/webpack.config.js
@@ -4,15 +4,18 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  //mode: 'development',
+  mode: 'production',
   entry: { 'main': './wwwroot/js/app.js' },
   output: {
     path: path.resolve(__dirname, 'wwwroot/dist'),
-    filename: 'aaha.profitability-simulator.js',
+    //filename: 'aaha.profitability-simulator.js',
+    filename: 'aaha.profitability-simulator.min.js',
     publicPath: 'dist/'
   },
   plugins: [
     new MiniCssExtractPlugin({
+        //filename: 'aaha.profitability-simulator.css',
         filename: 'aaha.profitability-simulator.min.css',
         minimize: true
     }),
@@ -20,7 +23,6 @@ module.exports = {
         $: 'jquery',
         jQuery: 'jquery',
         'window.jQuery': 'jquery',
-        Popper: ['popper.js', 'default']
     })
     ],
   optimization: {
@@ -32,8 +34,19 @@ module.exports = {
   },
   module: {
     rules: [ 
-        { test: /\.css$/, use: [ { loader: MiniCssExtractPlugin.loader, options: { minimize: true } }, "css-loader"] },
+        { test: /\.css$/, use: [ { loader: MiniCssExtractPlugin.loader, options: { minimize: true,  } }, "css-loader"] },
+        { test: /\.(png|woff|woff2|eot|ttf|svg)$/, use: { loader: 'url-loader?limit=100000', } },
         { test: /\.js?$/,  use: { loader: 'babel-loader', options: { presets: ['@babel/preset-env'] } } },
+        { test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+         use: [{
+           loader: 'file-loader',
+           options: {
+             name: '[name].[ext]',
+             outputPath: 'fonts/',    // where the fonts will go
+             publicPath: '../'       // override the default path
+           }
+         }] 
+        }
     ]
   }
 };
--- AAHA.ProfitabilitySimulator/wwwroot/js/profitability-simulator.js
+++ AAHA.ProfitabilitySimulator/wwwroot/js/profitability-simulator.js
@@ -7,6 +7,7 @@
   calcStaffSalaryWages();
   var cD8 = new Intl.NumberFormat().format(parseInt($('#d8').val().replace(/,/g, '')))
   $('#d8').val(cD8);
+  recalcAllScenarios();
 });
 $('#d9').change(function() {
   calcTransactionsPerFTEVeternarian();
@@ -14,39 +15,46 @@ $('#d9').change(function() {
   calcAnnualTransactionsPerActiveClient();
   var cD9 = new Intl.NumberFormat().format(parseInt($('#d9').val().replace(/,/g, '')))
   $('#d9').val(cD9);
+  recalcAllScenarios();
 });
 $('#d10').change(function() {
   calcRevenuePerFTEVeternarian();
   calcTransactionsPerFTEVeternarian();
   calcActiveClientsPerFTEVeternarian();
   var cD10 = new Intl.NumberFormat().format(parseInt($('#d10').val().replace(/,/g, '')))
-  $('#d10').val(cD10);
+  $('#d10').val(new Intl.NumberFormat().format(parseFloat((d10).toFixed(2))));
+  recalcAllScenarios();
 });
 $('#d11').change(function() {
   calcRevenuePerFTONonVeternarianEmp();
   var cD11 = new Intl.NumberFormat().format(parseInt($('#d11').val().replace(/,/g, '')))
-  $('#d11').val(cD11);
+  $('#d11').val(new Intl.NumberFormat().format(parseFloat((d11).toFixed(2))));
+  recalcAllScenarios();
 });
 $('#d12').change(function() {
   calcActiveClientsPerFTEVeternarian();
   calcAnnualTransactionsPerActiveClient();
   var cD12 = new Intl.NumberFormat().format(parseInt($('#d12').val().replace(/,/g, '')))
   $('#d12').val(cD12);
+  recalcAllScenarios();
 });
 $('#d13').change(function() {
   calcDrugsSuppliesExpense();
   var cD13 = new Intl.NumberFormat().format(parseInt($('#d13').val().replace(/,/g, '')))
   $('#d13').val(cD13);
+  recalcAllScenarios();
 });
 $('#d14').change(function() {
   calcLaboratoryExpense();
   var cD14 = new Intl.NumberFormat().format(parseInt($('#d14').val().replace(/,/g, '')))
   $('#d14').val(cD14);
+  recalcAllScenarios();
 });
 $('#d15').change(function() {
   calcStaffSalaryWages();
   var cD15 = new Intl.NumberFormat().format(parseInt($('#d15').val().replace(/,/g, '')))
   $('#d15').val(cD15);
+  recalcAllScenarios();
 });
 $('#d19').change(function() {
   calcPotentialEffectOnTotalIncome_1();
@@ -55,10 +63,20 @@ $('#d20').change(function() {
   calcPotentialEffectOnTotalIncome_2();
 });
 $('#d34').change(function() {
-  calcPotentialEffectOnTotalIncome_1();
+  if($('#d34').val()>0){
+    calcPotentialEffectOnTotalIncome_1();
+  }
+  else{
+    $('#d34').val("0");
+  }
 });
 $('#d39').change(function() {
-  calcPotentialEffectOnTotalIncome_2();
+  if($('#d39').val()>0){
+    calcPotentialEffectOnTotalIncome_2();
+  }
+  else{
+    $('#d39').val("0");
+  }
 });
 $('#d46').change(function() {
   calcPotentialEffectOnTotalIncome_3();
@@ -68,6 +86,14 @@ $('#d53').change(function() {
 });
 $('#d58').change(function() {
   calcPotentialEffectOnTotalIncome_5();
+});
+$('.aaha-profsim-enter').click(function() {
+    $('.aaha-profsim-results').removeClass('hide').addClass('show');
+    $('.aaha-profsim-main').removeClass('show').addClass('hide');
+});
+$('.aaha-profsim-exit').click(function() {
+    $('.aaha-profsim-results').removeClass('show').addClass('hide');
+    $('.aaha-profsim-main').removeClass('hide').addClass('show');
 });
 
 function calcRevenuePerFTEVeternarian(){
@@ -112,6 +138,23 @@ function calcAnnualTransactionsPerActiveClient(){
     $('#d23').val(new Intl.NumberFormat().format(parseFloat((d9 / d12).toFixed(2))));
   }
 }
+$(".aaha.card-header").click(function() {
+  var el = $(this).find("svg");
+  if(el.hasClass('fa-chevron-up')){ 
+    el.addClass('fa-chevron-down').removeClass('fa-chevron-up');
+  } else { 
+    el.addClass('fa-chevron-up').removeClass('fa-chevron-down'); 
+  }
+});
+$('div[data-toggle="collapse"]').click(function(e){
+    //e.stopPropagation();
+});
+function recalcAllScenarios(){
+  calcPotentialEffectOnTotalIncome_1();
+  calcPotentialEffectOnTotalIncome_2();
+  calcPotentialEffectOnTotalIncome_3();
+  calcPotentialEffectOnTotalIncome_4();
+}
 
 //CALC EXPENSES
 function calcDrugsSuppliesExpense(){
@@ -152,9 +195,10 @@ function calcPotentialEffectOnTotalIncome_2(){
     var d41 = parseFloat($('#d41').val().replace(/,/g, ''));
     var d26 = parseFloat($('#d26').val().replace(/,/g, ''));
     var d27 = parseFloat($('#d27').val().replace(/,/g, ''));
+    var d28 = parseFloat($('#d28').val().replace(/,/g, ''));
     $('#d41').val(new Intl.NumberFormat().format(Math.round(d39 * d20 * 52).toFixed(2)));
     var d41 = parseFloat($('#d41').val().replace(/,/g, ''));
-    $('#d42').val(new Intl.NumberFormat().format(Math.round(d41 * (1 - (d26/100) - (d27/100))).toFixed(2)));
+    $('#d42').val(new Intl.NumberFormat().format(Math.round(d41 * (1 - (d26/100) - (d27/100) - (d28/100))).toFixed(2)));
   }
 }
 function calcPotentialEffectOnTotalIncome_3(){
