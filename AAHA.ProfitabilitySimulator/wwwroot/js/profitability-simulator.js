3d17a1e8ae01fc3ba951fc3c38b980e2b4f04b19
Gabriel Jimenez <gabe@soonie.net>
2019-03-05 06:25:24 -0700

    ALPHA Update: [x] Update JS to add commas as user types for form values [x] Text changes [x] CSS changes to look and feel

--- AAHA.ProfitabilitySimulator/wwwroot/js/profitability-simulator.js
+++ AAHA.ProfitabilitySimulator/wwwroot/js/profitability-simulator.js
@@ -96,6 +96,41 @@ $('.aaha-profsim-exit').click(function() {
     $('.aaha-profsim-main').removeClass('hide').addClass('show');
 });
 
+$(".form-control").on('keyup', function(evt){
+  FormatCurrency(this);
+});
+$(".form-control").on('keypress', function(evt){
+  return CheckNumeric();
+});
+
+function FormatCurrency(ctrl) {
+    //Check if arrow keys are pressed - we want to allow navigation around textbox using arrow keys
+    if (event.keyCode == 37 || event.keyCode == 38 || event.keyCode == 39 || event.keyCode == 40) {
+        return;
+    }
+
+    var val = ctrl.value;
+
+    val = val.replace(/,/g, "")
+    ctrl.value = "";
+    val += '';
+    x = val.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? '.' + x[1] : '';
+
+    var rgx = /(\d+)(\d{3})/;
+
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+
+    ctrl.value = x1 + x2;
+}
+
+function CheckNumeric() {
+    return event.keyCode >= 48 && event.keyCode <= 57 || event.keyCode == 46;
+}
+
 function calcRevenuePerFTEVeternarian(){
   if($('#d8').val() !== '' && $('#d10').val() !== '' ){
     var d8 = parseInt($('#d8').val().replace(/,/g, ''));
